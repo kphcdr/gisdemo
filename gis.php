ec503a3fc7e104969a6a498867e0b05bf2e754d2
李虎头 <kphcdr@163.com>
2015-10-21 12:31:07 +0800

    ttt

--- gis.php
+++ gis.php
@@ -13,10 +13,11 @@ function p($var)
 class gis 
 {
 	private $model;
+	private $x;#当前精度116.40741300000002
+	private $y;#当前维度39.904214
 	public function __construct()
 	{
 		$this->m = new mysqli('127.0.0.1','root','root','geodatabase');
-		
 	}
 	
 	public function all()
@@ -27,10 +28,104 @@ class gis
 		while($a = $result->fetch_assoc()) {
 			$return[] = $a;
 		}
-		
-		
-		p($return);
-		
+			
 	}
 	
+	/**
+	 * 
+	 * @param 经度  $this->x
+	 * @param 维度  $this->y
+	 * @param 距离 $length
+	 */
+	public function area($x,$y,$level=1,$limit='0,10')
+	{
+		$this->x = $x;
+		$this->y = $y;
+		$area = $this->getOrthogon($level);
+		$areastr = "MBRContains(GeomFromText('Polygon((".$area['leftx']." ".$area['lefty'].",".$area['bomx']." ".$area['bomy'].",".$area['rightx']." ".$area['righty'].",".$area['topx']." ".$area['topy'].",".$area['leftx']." ".$area['lefty']."))')";
+		$sql = "SELECT id,AsText(pnt) from test where $areastr ,GeomFromText(AsText(pnt))) LIMIT $limit";
+		$result = $this->m->query($sql);
+		$return = [];
+		while($a = $result->fetch_assoc()) {
+			$return[] = $a;
+		}	
+		p($return);
+	}
+/* 	
+	*每隔0.00001度，距离相差约1米；
+	*每隔0.0001度，距离相差约10米；
+	*每隔0.001度，距离相差约100米；
+	*每隔0.01度，距离相差约1000米；
+	*每隔0.1度，距离相差约10000米。
+ */
+	public function getOrthogon($level)
+	{
+		//根据等级，画一个矩形
+		switch($level)
+		{
+			case 1:
+				$area['topx'] = $this->x;
+				$area['topy'] = $this->y + 0.01;
+				$area['bomx'] = $this->x;
+				$area['bomy'] = $this->y - 0.01;
+				$area['leftx'] = $this->x - 0.01;
+				$area['lefty'] = $this->y;
+				$area['rightx'] = $this->x + 0.01;
+				$area['righty'] = $this->y;
+				break;
+			case 2:
+				$area['topx'] = $this->x;
+				$area['topy'] = $this->y + 0.02;
+				$area['bomx'] = $this->x;
+				$area['bomy'] = $this->y - 0.02;
+				$area['leftx'] = $this->x - 0.02;
+				$area['lefty'] = $this->y;
+				$area['rightx'] = $this->x + 0.02;
+				$area['righty'] = $this->y;
+				break;
+			case 3:
+				$area['topx'] = $this->x;
+				$area['topy'] = $this->y + 0.05;
+				$area['bomx'] = $this->x;
+				$area['bomy'] = $this->y - 0.05;
+				$area['leftx'] = $this->x - 0.05;
+				$area['lefty'] = $this->y;
+				$area['rightx'] = $this->x + 0.05;
+				$area['righty'] = $this->y;
+				break;
+			case 4:
+				$area['topx'] = $this->x;
+				$area['topy'] = $this->y + 0.1;
+				$area['bomx'] = $this->x;
+				$area['bomy'] = $this->y - 0.1;
+				$area['leftx'] = $this->x - 0.1;
+				$area['lefty'] = $this->y;
+				$area['rightx'] = $this->x + 0.1;
+				$area['righty'] = $this->y;
+				break;
+			case 5:
+				$area['topx'] = $this->x;
+				$area['topy'] = $this->y + 1;
+				$area['bomx'] = $this->x;
+				$area['bomy'] = $this->y - 1;
+				$area['leftx'] = $this->x - 1;
+				$area['lefty'] = $this->y;
+				$area['rightx'] = $this->x + 1;
+				$area['righty'] = $this->y;
+				break;
+		}
+		return $area;		
+	}
+	
+	public function addTestdata()
+	{
+		set_time_limit(0);
+		$i = 0;
+		while($i < 100000)
+		{
+			$sql = "insert into test values(null,GeomFromText('POINT(".mt_rand(-100,100)." ".mt_rand(-100,100).")'));";
+			$return = $this->m->query($sql);
+			$i++;
+		}
+	}
 }
--- index.php
+++ index.php
@@ -4,5 +4,17 @@ include 'gis.php';
 
 
 $gis = new gis();
-$gis->all();
+$gis->addTestdata();
+
+/* MBRContains(g1,g2)
+
+返回1或0以指明g1的最小边界矩形是否包含g2的最小边界矩形。
+mysql> SET @g1 = GeomFromText('Polygon((0 0,0 3,3 3,3 0,0 0))');
+mysql> SET @g2 = GeomFromText('Point(1 1)');
+mysql> SELECT MBRContains(@g1,@g2), MBRContains(@g2,@g1);
+----------------------+----------------------+
+| MBRContains(@g1,@g2) | MBRContains(@g2,@g1) |
++----------------------+----------------------+
+|                    1 |                    0 |
++----------------------+----------------------+ */
 ?>
